16f16b3a98b4d09a25de8145ff67723b680a4409
Berlinda Babe <Berlinda@Beibeis-MacBook.local>
2023-05-11 12:26:10 -0400

    first commit

---
+++ src/main/java/com/example/SpringSecurityDemo/UserService.java
@@ -0,0 +1,30 @@
+package com.example.SpringSecurityDemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public List<User> getUser() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+}
---
+++ src/main/java/com/example/SpringSecurityDemo/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package com.example.SpringSecurityDemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    //authentication
+    @Bean
+    public UserDetailsService userDetailsService(){
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+//        UserDetails admin = User.withUsername("Bella")
+//                .password(encoder.encode("bella"))
+//                .roles("ADMIN")
+//                .build();
+//        UserDetails user = User.withUsername("Zack")
+//                .password(encoder.encode("zack"))
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+        return new myUserDetailsService();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //authorization
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http.csrf().disable()
+//                .authorizeHttpRequests()
+//                .requestMatchers("/","/users/all","/users/add").permitAll()
+//                .and()
+//                .authorizeHttpRequests().requestMatchers("/users/**")
+//                .authenticated()
+//                .and().formLogin()
+////                .loginPage("login")
+//                .and().build();
+        return http.csrf().disable()
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/", "/users/all", "/users/add").permitAll()
+                        .requestMatchers("/users/id/**").hasRole("ADMIN")
+                        .requestMatchers("/users/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+
+                .formLogin()
+                .loginPage("/login")
+                .and().build();
+
+
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+}
